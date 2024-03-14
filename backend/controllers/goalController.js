552c2e6e6222da50ff8aa1b5bdb9a1264a0198d5
LAKSHMAN S <lakshmans218@gmail.com>
2024-03-14 20:57:49 +0530

    mongoose connected

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,34 +1,42 @@
+const asyncHandler = require("express-async-handler")
+
+const Goal = require('../model/goalModel')
+
 // get goals
 // get/api/goals
 // access private
-const getGoals = (req, res) => {
-  res.status(200).json({message: 'Get goals'})
-}
+const getGoals = asyncHandler(async (req, res) => {
+  const goals = await Goal.find()
+  res.status(200).json(goals)
+})
 
 // set goals
 // set/api/goals
 // access private
-const setGoals = (req, res) => {
+const setGoals = asyncHandler(async (req, res) => {
   if(!req.body.text) {
     res.status(400)
     throw new Error('please add a text field')
   }
-  res.status(200).json({message: 'Set goals'})
-}
+  const goal = await Goal.create({
+    text: req.body.text
+  })
+  res.status(200).json(goal)
+})
 
 // update goals
 // update/api/goals
 // access private
-const updateGoals = (req, res) => {
+const updateGoals = asyncHandler(async (req, res) => {
   res.status(200).json({message: `Update goals ${req.params.id}`})
-}
+})
 
 // delete goals
 // delete/api/goals
 // access private
-const deleteGoals = (req, res) => {
+const deleteGoals = asyncHandler(async (req, res) => {
   res.status(200).json({message: `Delete goals ${req.params.id}`})
-}
+})
 
 module.exports = {
   getGoals, setGoals, updateGoals, deleteGoals
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const colors = require('colors')
 const port = process.env.PORT || 5000
+const {errorHandler} = require('./middleware/errorMiddleware')
 const app = express()
+const connectDB = require('./config/db')
+connectDB()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.use('/api/goals', require('./routes/goalRoutes'))
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`server started on port ${port}`)) 
